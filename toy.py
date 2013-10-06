188d32ccefc89e69f6d32c99a00f75aded702108
alklein <alklein@stanford.edu>
2013-10-06 19:53:11 -0400

    corrected use of mu and sigma

--- toy.py
+++ toy.py
@@ -20,26 +20,37 @@ class toyData:
 
         self.M = M
         self.eta = eta
-        
-        # mu_1, mu_2 ~ Unif[0, 1]
-        self.mu_1 = np.random.rand()
-        self.mu_2 = np.random.rand()
-
-        # sig_1, sig_2 ~ Unif[0.05, 0.10]
-        self.sig_1 = .05*(np.random.rand() + 1)
-        self.sig_2 = .05*(np.random.rand() + 1)
-
         self.data = None
 
         if verbose:
             print '\n >>> [verbose] New toyData object made with M =',str(M) + ',','eta =',eta,'\n'
         
     """
-    Makes M <input, output> distribution pairs.
+    Makes M <input, output> distribution pairs. 
     Each is sampled eta times.
+
+    Training instance i uses input values drawn from the pdf p_i
+    and output values drawn from the pdf q_i.
+
+    Returns a length-M list of training instances; 
+    each instance is a tuple of the form [in, out]_i,     
+    where in and out are length-eta lists of scalar values.
     """
     def make_data(self):
         data = []
+        for i in range(self.M):
+            
+            # mu_1, mu_2 ~ Unif[0, 1]
+            mu_1 = np.random.rand()
+            mu_2 = np.random.rand()
+            
+            # sig_1, sig_2 ~ Unif[0.05, 0.10]
+            sig_1 = .05*(np.random.rand() + 1)
+            sig_2 = .05*(np.random.rand() + 1)
+
+            # TODO: create and append new instance
+
+        data = np.array(data)
         self.data = data
         return data
 
@@ -48,10 +59,8 @@ class toyData:
     """
     def print_params(self):
         print
-        print ' >>> [debug] mu_1:',self.mu_1
-        print ' >>> [debug] mu_2:',self.mu_2
-        print ' >>> [debug] sig_1:',self.sig_1
-        print ' >>> [debug] sig_2:',self.sig_2
+        print ' >>> [debug] M:',self.M
+        print ' >>> [debug] eta:',self.eta
         print
 
 
@@ -69,5 +78,5 @@ if __name__ == '__main__':
     tD.print_params()
     print ' > [debug] Generating toy data'
     data = tD.make_data()
-    print ' >>> [debug] Length of toy data:',len(data)
+    print ' > [debug] Length of toy data:',len(data)
     print
