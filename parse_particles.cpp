f0afbda0acb40fb8aa46767fc577594b03c4b2a9
Andrea Klein <andreakl@lov4.int.autonlab.org>
2013-11-25 19:44:24 -0500

    added parsing script for simulation data

---
+++ parse_particles.cpp
@@ -0,0 +1,67 @@
+/*
+
+File: parse_particles.cpp
+Brief: Script to extract particles from binary simulation output file
+Author: Andrea Klein       <alklein@alumni.stanford.edu>
+
+Usage: g++ parse_particles.cpp -o parser
+       ./parser > particles.txt
+Note: the infile must be in the same directory.
+
+*/
+
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// Np = number of particles assigned to halos.
+// Total number of particles is ~ 1 billion?
+int Np = 10000000;
+
+struct _particle
+{
+  int ip;
+  int ih;
+  float x, y, z;
+  float vx, vy, vz;
+};
+
+int readsim(string fname, _particle *pp)
+{
+
+  ifstream infile;
+  infile.open(fname.c_str(), ios::binary|ios::in);
+  double * dummy = new double[1];
+  infile.read( (char *)&dummy[0], 8 ); // read first 8 bytes into dummy
+
+  for(int i=0; i<Np; i++)
+    {
+      infile.read( (char *)&pp[i].ip, 8 );
+      infile.read( (char *)&pp[i].ih, 8 );
+      infile.read( (char *)&pp[i].x, sizeof(float) );
+      infile.read( (char *)&pp[i].y, sizeof(float) );
+      infile.read( (char *)&pp[i].z, sizeof(float) );
+      infile.read( (char *)&pp[i].vx, sizeof(float) );
+      infile.read( (char *)&pp[i].vy, sizeof(float) );
+      infile.read( (char *)&pp[i].vz, sizeof(float) );
+    }
+
+  return 0;
+}
+
+int main()
+{
+  _particle *pp = new _particle[Np];
+  readsim("xv_dm.z=00.0000_sim1", pp); // change infile here
+
+  for (int i=0; i<Np; i++) {
+    cout << pp[i].ip << ' ' 
+         << pp[i].ih << ' '
+         << pp[i].x  << ' ' << pp[i].y << ' ' << pp[i].z << ' '
+         << pp[i].vx << ' ' << pp[i].vy << ' ' << pp[i].vz << endl;
+  }
+
+  return 0;
+}
